29cd260b5745e7d85d8196ade2db8e5676e0589e
andrew21-mch <nfonandrew73@gmail.com>
2022-06-18 14:41:31 +0100

    userAuthmysql solution

--- userAuth/index.php
+++ userAuth/index.php
@@ -9,27 +9,27 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
-<body>
+<body class="body">
 
     <!-- Image and text -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><h2>ZURI-PHP</h2></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item f-right">
-        <a class="nav-link" href="forms/login.html">Login</a>
-      </li>
-      <li class="nav-item f-right">
-        <a class="nav-link " href="forms/register.html">Register</a>
-      </li>
-  </div>
-</nav>
-<div class="container justify-content-center">
-     <h1 class="">Welcome to Zuri Authentication with PHP</h1>
-</div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="index.php"><h2>ZURI-PHP</h2></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item f-right">
+            <a class="nav-link" href="forms/login.html">Login</a>
+          </li>
+          <li class="nav-item f-right">
+            <a class="nav-link " href="forms/register.html">Register</a>
+          </li>
+      </div>
+    </nav>
+    <div class="container justify-content-center" style="margin-top: 5%;">
+        <h1 class="">Welcome to Zuri Student Management Portal with PHP and MySQL</h1>
+    </div>
    
 </body>
 </html>
