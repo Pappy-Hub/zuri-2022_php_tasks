21103aace80d2ad3399860d28ac7f6cfc1c3e4d4
andrew21-mch <nfonandrew73@gmail.com>
2022-06-18 13:32:06 +0100

    userAuthmysql solution

--- userAuth/php/userauth.php
+++ userAuth/php/userauth.php
@@ -78,10 +78,11 @@ function getusers(){
                 "</td> <td style='width: 150px'>" . $data['gender'] . 
                 "</td> <td style='width: 150px'>" . $data['country'] . 
                 "</td>
+                <td style='width: 150px'> 
                 <form action='action.php' method='post'>
                 <input type='hidden' name='id'" .
                  "value=" . $data['id'] . ">".
-                "<td style='width: 150px'> <button type='submit', name='delete'> DELETE </button>".
+                "<button type='submit', name='delete'> DELETE </button> </form> </td>".
                 "</tr>";
         }
         echo "</table></table></center></body></html>";
