fbc5a1bf1464c340c9ebd198d3821ed5743317ca
andrew21-mch <nfonandrew73@gmail.com>
2022-06-18 18:04:50 +0100

    userauth with files an functions

--- userAuth/php/logout.php
+++ userAuth/php/logout.php
@@ -1,10 +1,21 @@
 <?php
 function logout(){
-    /*
+  
+   session_start();
+   if($_SESSION['username']){
+       session_destroy();
+       header('Location: ../index.php');
+   }
+ 
+   else{
+       echo "You are not logged in";
+   }
+   /*
 Check if the existing user has a session
 if it does
 destroy the session and redirect to login page
 */
 }
-
-echo "HANDLE THIS PAGE";
+ 
+logout();
+ 
--- userAuth/php/register.php
+++ userAuth/php/register.php
@@ -1,18 +1,32 @@
 <?php
 if(isset($_POST['submit'])){
-    $username = $_POST['fullname'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
+   $username = $_POST['fullname'];
+   $email = $_POST['email'];
+   $password = $_POST['password'];
 registerUser($username, $email, $password);
-
 }
-
 function registerUser($username, $email, $password){
-    //save data into the file
-    
-    // echo "OKAY";
-}
-echo "HANDLE THIS PAGE";
-
-
+   $form_data = array(
+       'fullname' => $username,
+       'email' => $email,
+       'password' => $password );
+   $exist = checkIfUserExist($email);
+   if($exist){
+       echo "User already exist";
+   }else{
+       $file = fopen('../storage/users.csv', 'a');
+       fputcsv($file, $form_data);
+       fclose($file);
+       echo "User registered";
+   }}
+function checkIfUserExist($email){
+       $file = fopen('../storage/users.csv', 'r');
+       while(!feof($file)){
+           $line = fgetcsv($file);
+           if($line[1] == $email){
+               return true;
+           }
+       }
+       fclose($file);
+       return false;
+   }
